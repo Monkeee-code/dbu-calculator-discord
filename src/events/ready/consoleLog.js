091465913390df6cbb72739dc3728afd9a003532
Monkeee-code <markokuruzovic9@gmail.com>
2025-07-29 16:25:59 +0200

    Console Log

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -3,13 +3,13 @@ const { ActivityType } = require('discord.js');
 module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
     client.user.setActivity({ name: "/help | Check our Website", type: ActivityType.Listening, });
-
-    let now = new Date();
+    let now1 = new Date();
     setTimeout(() => {
+        let now = new Date();
         let hours = now.getHours();
         let minutes = now.getMinutes();
         let seconds = now.getSeconds();
         console.log(`[${hours}:${minutes}:${seconds}] Code is running...`);
     }, 3600000);
-    console.log(`[${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] Code is ruunning... First Run.`);
+    console.log(`[${now1.getHours()}:${now1.getMinutes()}:${now1.getSeconds()}] Code is ruunning... First Run.`);
 };
