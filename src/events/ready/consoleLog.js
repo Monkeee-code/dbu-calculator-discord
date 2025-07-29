a22b97c96e46c9e4ba94691f2d32cb868bea3e71
Monkeee-code <markokuruzovic9@gmail.com>
2025-07-29 22:33:25 +0200

    console log

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
     client.user.setActivity({ name: "/help | Check our Website", type: ActivityType.Listening, });
     let now1 = new Date();
-    setTimeout(() => {
+    setInterval(() => {
         let now = new Date();
         let hours = now.getHours();
         let minutes = now.getMinutes();
